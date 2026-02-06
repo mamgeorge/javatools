cfedf66660f0beb4d9d586a52ab21da8785112e0
mamgeorge <mamgeorge@yahoo.com>
2026-02-05 19:23:24 -0500

    added Jsoup

---
+++ src/test/java/utils/SpecialParse.java
@@ -0,0 +1,127 @@
+package utils;
+
+import jakarta.annotation.Nonnull;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class SpecialParse {
+
+	public static final String EOL = System.lineSeparator();
+	private static final String[] TXT_URL = { "https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub",
+	"https://docs.google.com/document/d/e/2PACX-1vSvM5gDlNvt7npYHhp_XfsJvuntUhq184By5xO_pA4b_gCWeXb6dM6ZxwN8rE6S4ghUsCj2VKR21oEP/pub"};
+	public static void main(String[] args) {
+
+		String txtDoc = getDoc(TXT_URL[1]);
+		String parsedDoc = parsedDoc(txtDoc);
+		String renderDoc = renderDoc(parsedDoc);
+
+		System.out.println(renderDoc);
+		System.out.println("DONE");
+	}
+
+	private static String getDoc(String textUrl) {
+
+		StringBuilder sb = new StringBuilder();
+		try {
+			URL url = new URL(textUrl);
+			InputStream inputStream = url.openStream();
+			InputStreamReader ISR = new InputStreamReader(url.openStream(), UTF_8);
+			BufferedReader buffRead = new BufferedReader(ISR);
+
+			String inputLine;
+			while ((inputLine = buffRead.readLine()) != null) {
+				sb.append(inputLine);
+				sb.append(System.lineSeparator());
+			}
+			buffRead.close();
+
+		} catch (IOException ex) {
+			System.out.println("ERROR: " + ex.getMessage());
+		}
+		return sb.toString();
+	}
+
+	private static String parsedDoc(String txtDoc) {
+
+		StringBuilder sb = new StringBuilder();
+		Document doc = Jsoup.parse(txtDoc);
+		Element table = doc.select("table").first();
+		Elements rowTrs = table.select("tr");
+
+		for (Element rowTds : rowTrs) {
+			Elements cells = rowTds.select("td");
+			for (Element cell : cells) {
+				sb.append(cell.text() + "\t");
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+
+	private static String renderDoc(String parsedDoc) {
+
+		String[] rowsOrg = parsedDoc.split("\n");
+		String[] rows = Arrays.copyOfRange(rowsOrg, 1, rowsOrg.length);
+		int rowlen = rowsOrg.length - 1;
+
+		// create matrix & fill
+		String[] qval = new String[rowlen];
+		int ictr = 0;
+		int rctr = rowlen;
+		for (String row : rows) {
+
+			String[] cols = row.split("\t");
+			qval[ictr] = Integer.parseInt(cols[2]) + "," + rctr--
+					+ "," + Integer.parseInt(cols[0]) + "," + cols[1].trim();
+			ictr++;
+		}
+		Arrays.sort(qval);
+		Collections.reverse(Arrays.asList(qval));
+
+		String lines = remapArrays(qval, rowlen);
+		return lines;
+	}
+
+	@Nonnull
+	private static String remapArrays(String[] qval, int rowlen) {
+
+		StringBuilder sb = new StringBuilder();
+		int yitm = 0;
+		String citm = "";
+		String lines = "";
+		int yctr = Integer.parseInt(qval[0].split(",")[0]);
+		for (int qctr = 0; qctr < rowlen; ) {
+
+			yitm = Integer.parseInt(qval[qctr].split(",")[0]);
+			citm = qval[qctr].split(",")[3];
+
+			if (yitm == yctr) {
+				sb.append(citm);
+				lines+=citm;
+				if (qctr==(rowlen -1) && lines.length()<4)
+				{ sb.append(" ".repeat(4-lines.length())); }
+			} else {
+				yctr = yitm;
+				//System.out.println(line);
+				if (lines.length()<4) { sb.append(" ".repeat(4-lines.length())); }
+				sb.append("\n");
+				lines = "";
+				qctr--;
+			}
+			qctr++;
+		}
+		return sb.toString();
+	}
+}
--- src/test/java/utils/SpecialTests.java
+++ src/test/java/utils/SpecialTests.java
@@ -20,11 +20,28 @@ import static utils.UtilityMainTest.PATHFILE_LOCAL;
 
 class SpecialTests {
 
-	@Test void test_error( ) {
+
+	//#### statics
+	private static Configuration getSphinxConfig() {
+		//
+		String pathAcousticModel = "resource:/edu/cmu/sphinx/models/en-us/en-us";
+		String pathDictionary = "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict";
+		String pathLanguageModel = "resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin";
+		//
+		Configuration configuration = new Configuration();
+		configuration.setAcousticModelPath(pathAcousticModel);
+		configuration.setDictionaryPath(pathDictionary);
+		configuration.setLanguageModelPath(pathLanguageModel);
+		return configuration;
+	}
+
+	@Test
+	void test_error() {
 		//
 		int intVal = 0;
-		try { intVal = 1 / 0; }
-		catch (ArithmeticException ex) {
+		try {
+			intVal = 1 / 0;
+		} catch (ArithmeticException ex) {
 			System.out.println("ERROR ex.toString(): " + ex);
 			System.out.println("ERROR ex.getMessage(): " + ex.getMessage());
 			System.out.println("ERROR ex.getLocalizedMessage(): " + ex.getLocalizedMessage());
@@ -33,7 +50,7 @@ class SpecialTests {
 			int ictr = 0;
 			StackTraceElement[] STEs = ex.getStackTrace();
 			System.out.println("ERROR ex.getStackTrace(): " + STEs.length);
-			for ( StackTraceElement STE : STEs ) {
+			for (StackTraceElement STE : STEs) {
 				txtLines += String.format("\t %02d %s\n", ++ictr, STE.toString());
 			}
 			System.out.println(txtLines);
@@ -43,7 +60,9 @@ class SpecialTests {
 		assertTrue(intVal > -1);
 	}
 
-	@Test @Disabled( "too slow" ) void test_sphinx4_STT( ) {
+	@Test
+	@Disabled("too slow")
+	void test_sphinx4_STT() {
 		//
 		// https://cmusphinx.github.io/wiki/tutorialsphinx4/
 		String pathFile = PATHFILE_LOCAL + "hal9000.wav";
@@ -56,16 +75,19 @@ class SpecialTests {
 			File file = new File(pathFile);
 			InputStream FIS = new FileInputStream(file);
 			ssRecognizer.startRecognition(FIS);
-			while ( ( speechResult = ssRecognizer.getResult() ) != null ) {
+			while ((speechResult = ssRecognizer.getResult()) != null) {
 				System.out.format("Hypothesis: %s\n", speechResult.getHypothesis());
 			}
 			ssRecognizer.stopRecognition();
+		} catch (IOException ex) {
+			System.out.println("ERROR: " + ex.getMessage());
 		}
-		catch (IOException ex) { System.out.println("ERROR: " + ex.getMessage()); }
 		assertNotNull(speechResult, ASSERT_MSG);
 	}
 
-	@Test @Disabled( "too slow" ) void test_sphinx4_TTS_SS( ) {
+	@Test
+	@Disabled("too slow")
+	void test_sphinx4_TTS_SS() {
 		//
 		// https://cmusphinx.github.io/wiki/tutorialsphinx4/
 		String pathFile = PATHFILE_LOCAL + "hal9000.wav";
@@ -81,15 +103,18 @@ class SpecialTests {
 			speechResultSS = ssRecognizer.getResult();
 			ssRecognizer.stopRecognition();
 			System.out.println(speechResultSS.getHypothesis());
-			for ( WordResult wordResult : speechResultSS.getWords() ) {
+			for (WordResult wordResult : speechResultSS.getWords()) {
 				System.out.println(wordResult);
 			}
+		} catch (IOException ex) {
+			System.out.println("ERROR: " + ex.getMessage());
 		}
-		catch (IOException ex) { System.out.println("ERROR: " + ex.getMessage()); }
 		assertNotNull(speechResultSS, ASSERT_MSG);
 	}
 
-	@Test @Disabled( "too slow" ) void test_sphinx4_TTS_LS( ) {
+	@Test
+	@Disabled("too slow")
+	void test_sphinx4_TTS_LS() {
 		//
 		// https://cmusphinx.github.io/wiki/tutorialsphinx4/
 		// String pathFile = PATHFILE_LOCAL + "hal9000.wav";
@@ -105,43 +130,33 @@ class SpecialTests {
 			speechResultLS = lsRecognizer.getResult();
 			lsRecognizer.stopRecognition();
 			System.out.println(speechResultLS.getHypothesis());
-			for ( WordResult wordResult : speechResultLS.getWords() ) {
+			for (WordResult wordResult : speechResultLS.getWords()) {
 				System.out.println(wordResult);
 			}
+		} catch (IOException ex) {
+			System.out.println("ERROR: " + ex.getMessage());
 		}
-		catch (IOException ex) { System.out.println("ERROR: " + ex.getMessage()); }
 		assertNotNull(speechResultLS, ASSERT_MSG);
 	}
 
-	@Test void listFiles( ) {
+	@Test
+	void listFiles() {
 
 		// C:\Users\mamge\OneDrive\Documents\5Personal\History\images_levant_extra
 		String folderPath = "C:/Users/mamge/OneDrive/Documents/"
-			+ "5Personal/History/images_levant10";
+				+ "5Personal/History/images_levant10";
 
 		File folder = new File(folderPath);
 		File[] files = folder.listFiles();
 
-		for ( File file : files ) {
-			if ( file.isFile() ) { System.out.println("File " + file.getName()); } else {
+		for (File file : files) {
+			if (file.isFile()) {
+				System.out.println("File " + file.getName());
+			} else {
 				System.out.println("file is: " + file);
 			}
 		}
 
 		assertNotNull(files);
-	}
-
-	//#### statics
-	private static Configuration getSphinxConfig( ) {
-		//
-		String pathAcousticModel = "resource:/edu/cmu/sphinx/models/en-us/en-us";
-		String pathDictionary = "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict";
-		String pathLanguageModel = "resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin";
-		//
-		Configuration configuration = new Configuration();
-		configuration.setAcousticModelPath(pathAcousticModel);
-		configuration.setDictionaryPath(pathDictionary);
-		configuration.setLanguageModelPath(pathLanguageModel);
-		return configuration;
 	}
 }
